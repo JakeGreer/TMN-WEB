ed16d2e1ac3bee42ceb0148eda1194e050d9e518
Jake Greer <jakegreer11@gmail.com>
2018-10-02 23:31:51 -0700

    testing out some styles on login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,33 +1,40 @@
 <template>
-  <div class="row">
-    <div class="col-6">
-      <form @submit-prevent="submitLogin">
-        <div class="row">
-            <div class="col-12">
-                <div class="login-panel">
-                  <h3>Login</h3>
-                </div> 
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 mx-auto login-component">
+        <form @submit-prevent="submitLogin">
+          <div class="row">
+              <div class="col-md-12">
+                  <div class="login-panel text-center">
+                    <h3>Login</h3>
+                  </div> 
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-12 text-center">
+                  <input ref="emailText" type="text" class="input" v-bind:class="[emailSuccess]" name="username" placeholder="Email/Username" @input="validateEmail" autofocus required />
+                  <span><i v-bind:class="[emailIcon]"></i></span>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-12 text-center">
+                  <input ref="passwordText" type="password" class="input" v-bind:class="[passwordSuccess]" name="password" placeholder="Password" @input="validatePassword" pattern=".{6,25}" required />
+                  <span><i v-bind:class="[passwordIcon]"></i></span>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-12 text-center">
+                  <span class="error" v-if="errorMessage">{{errorMessage}}</span>
+                  <button class="btn login-button" :disabled="submitDisabled">Sign In</button>
+              </div>
+          </div>
+        </form>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <img id="landing-background" src="/public/images/music-background.jpg" />
         </div>
-        <div class="row">
-            <div class="col-12">
-                <input ref="emailText" type="text" class="input" v-bind:class="[emailSuccess]" name="username" placeholder="Email/Username" @input="validateEmail" autofocus required />
-                <span><i v-bind:class="[emailIcon]"></i></span>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <input ref="passwordText" type="password" class="input" v-bind:class="[passwordSuccess]" name="password" placeholder="Password" @input="validatePassword" pattern=".{6,25}" required />
-                <span><i v-bind:class="[passwordIcon]"></i></span>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <span class="error" v-if="errorMessage">{{errorMessage}}</span>
-                <button class="btn" :disabled="submitDisabled">Sign In</button>
-            </div>
-        </div>
-      </form>
+      </div>
     </div>
   </div>
 </template>
@@ -83,25 +90,63 @@ export default {
         password: password
       });
     }
+  },
+  mounted() {
+    this.$refs.videoElement.playbackRate = 0.2;
   }
 };
 </script>
 
 <style lang="scss" >
 @import "styles/font-awesome/scss/_variables.scss";
+@import "styles/index.scss";
 
-.login-panel {
-  position: relative;
-  display: -ms-flexbox;
-  display: flex;
-  -ms-flex-direction: column;
-  flex-direction: column;
-  min-width: 0;
-  word-wrap: break-word;
-  background-color: #fff;
-  background-clip: border-box;
-  border: 1px solid rgba(0, 0, 0, 0.125);
-  border-radius: 0.25rem;
+#landing-background {
+  min-height: 100%;
+  min-width: 1024px;
+  width: 100%;
+  height: auto;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+
+.login {
+  &-component {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    background: #fff;
+  }
+  &-button {
+    background: $primary-color;
+    color: black;
+    border-radius: 0;
+    box-shadow: none;
+    border: none;
+    font-weight: bold;
+    padding: 10px 15px;
+    margin-bottom: 25px;
+  }
+  &-panel {
+    position: relative;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
+    background-clip: border-box;
+    border: 1px solid rgba(0, 0, 0, 0.125);
+    border-radius: 0.25rem;
+    margin-bottom: 25px;
+    background: $primary-color;
+    margin-left: -15px;
+    margin-right: -15px;
+  }
 }
 .error {
   color: "#b60505";
